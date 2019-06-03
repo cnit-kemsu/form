4015bda81a3a3f93236f01bed738b56531f54745
John Cooper <johnxcooper87@gmail.com>
2019-06-03 11:50:35 +0700

    update

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ export { useComposite } from './src/hooks/useComposite';
 export { useFieldArray } from './src/hooks/useFieldArray';
 export { useArrayElement } from './src/hooks/useArrayElement';
 export { useFormSubscriber } from './src/hooks/useFormSubscriber';
-export { default as Fields } from './src/components/Fields';
+export { default as Fields, ComposerContext } from './src/components/Fields';
